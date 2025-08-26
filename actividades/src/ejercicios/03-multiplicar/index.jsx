5a1b6cc32fc2b2f6afb284fb789fbd4e44395030
tiagooo0 <thurstaltamirano@escuelasproa.edu.ar>
2025-08-26 12:25:04 -0300

    Refactor multiplication component to remove unused state and simplify calculation

--- actividades/src/ejercicios/03-multiplicar/index.jsx
+++ actividades/src/ejercicios/03-multiplicar/index.jsx
@@ -1,14 +1,12 @@
 import { useState } from "react";
 
 export default function EjMultiplicar() {
-  const [a, setA] = useState(0);
-  const [b, setB] = useState(0);
+  const [a, setA] = useState();
   return (
     <div>
       <h2>Ej 03 – Multiplicar</h2>
       <input type="number" value={a} onChange={(e) => setA(e.target.value)} />
-      <input type="number" value={b} onChange={(e) => setB(e.target.value)} />
-      <p>Resultado: {a * b}</p>
+      <p>Resultado: {a * 2}</p>
     </div>
   );
 }
